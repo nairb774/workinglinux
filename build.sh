69f7c2bfff94ff95bde5035669fe724c7697a0f8
Brian Atkinson <brian@atkinson.mn>
2023-04-02 17:36:53 -0700

    feat(build): Tag the images with the git commit.
    This can help with knowing where a particular image was built from, and
    allows for some level of rollback.

--- build.sh
+++ build.sh
@@ -13,11 +13,15 @@ ssh-add -L \
 
 gpg --export --armor > context/generated/gpg-public-keys.asc
 
-exec limactl shell workinglinux sudo nerdctl build \
+COMMIT="$(git rev-parse HEAD)"
+
+limactl shell workinglinux sudo nerdctl build \
   --progress=plain \
   --build-arg=USER="$USER" \
-  --label git-commit="$(git rev-parse HEAD)" \
+  --label git-commit="$COMMIT" \
   --file "$PWD/Dockerfile" \
-  --tag nairb774/workinglinux \
+  --tag "nairb774/workinglinux:$COMMIT" \
   --target img \
   "$PWD/context"
+
+limactl shell workinglinux sudo nerdctl image tag "nairb774/workinglinux:$COMMIT" "nairb774/workinglinux:latest"
